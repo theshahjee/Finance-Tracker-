a721b4d2af6d36dadbb99c6e3c7ae06b05c5a7f2
ChetanR01 <chetan0101career@gmail.com>
2023-09-12 23:54:13 +0530

    Corresponding user for each income,exp,acc

--- FinTech/admin.py
+++ FinTech/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import IncomeCategory,ExpenseCategory,Income,Expense
+from .models import IncomeCategory,ExpenseCategory,Income,Expense,Account
 
 # Register your models here.
 admin.site.register(IncomeCategory)
@@ -7,14 +7,19 @@ admin.site.register(ExpenseCategory)
 
 # TO Modify INcome page for admin
 class IncomeAdmin(admin.ModelAdmin):
-    list_display = ['name','date','amount','category','note']
-    search_fields = ('name', 'category')
-    list_filter = ('category','date')
+    list_display = ['user','name','date','amount','category','note']
+    search_fields = ('user','name', 'category')
+    list_filter = ('user','category','date')
 admin.site.register(Income, IncomeAdmin)
 
 # TO Modify Expenses page for admin
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ['name','date','amount','category','note']
-    search_fields = ('name', 'category')
-    list_filter = ('category','date')
+    list_display = ['user','name','date','amount','category','note']
+    search_fields = ('user','name', 'category')
+    list_filter = ('user','category','date')
 admin.site.register(Expense, ExpenseAdmin)
+
+# TO Modify Account page for admin
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['user','name','balance']
+admin.site.register(Account, AccountAdmin)
