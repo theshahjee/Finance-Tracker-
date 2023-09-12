a895966f78f5c099e99822f48317cb478a7f5e45
ChetanR01 <chetan0101career@gmail.com>
2023-09-12 22:46:36 +0530

    initial commit + signup and login

---
+++ FinTech/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('dashboard',views.dashboard,name="dashboard"),
+
+
+
+    path('',views.index,name="index"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.login,name="login"),
+]
