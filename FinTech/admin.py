cfc8c8a0c8270df4baaff3e5d4eea8186289a7dd
ChetanR01 <chetan0101career@gmail.com>
2023-09-12 23:14:40 +0530

    income.expense,catg models

--- FinTech/admin.py
+++ FinTech/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import IncomeCategory,ExpenseCategory,Income,Expense
 
 # Register your models here.
+admin.site.register(IncomeCategory)
+admin.site.register(ExpenseCategory)
+
+# TO Modify INcome page for admin
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ['name','date','amount','category','note']
+    search_fields = ('name', 'category')
+    list_filter = ('category','date')
+admin.site.register(Income, IncomeAdmin)
+
+# TO Modify Expenses page for admin
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['name','date','amount','category','note']
+    search_fields = ('name', 'category')
+    list_filter = ('category','date')
+admin.site.register(Expense, ExpenseAdmin)
