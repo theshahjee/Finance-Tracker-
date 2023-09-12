d580c3f7b400342708643cefac6d24f27eaa3767
ChetanR01 <chetan0101career@gmail.com>
2023-09-12 23:14:40 +0530

    income.expense,catg models

--- FinTech/models.py
+++ FinTech/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from datetime import date, datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+# For Income Categories
+class IncomeCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+# For Expense Categories
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+class Income(models.Model):
+    name= models.CharField(max_length=50)
+    date = models.DateTimeField(default=datetime.now())
+    amount= models.FloatField()
+    category = models.ForeignKey(IncomeCategory, related_name='income_catg', on_delete=models.CASCADE)
+    note=models.TextField()
+
+    def __str__(self):
+        return f"Income of Rs.{self.amount} for {self.name}"
+
+
+class Expense(models.Model):
+    name= models.CharField(max_length=50)
+    date = models.DateTimeField(default=datetime.now())
+    amount= models.FloatField()
+    category = models.ForeignKey(ExpenseCategory, related_name='expense_catg', on_delete=models.CASCADE)
+    note=models.TextField()
+
+    def __str__(self):
+        return f"Rs.{self.amount} spent for {self.name}"
