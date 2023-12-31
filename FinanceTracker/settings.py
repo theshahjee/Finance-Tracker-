32b4287633164792f04e0f52c48bd4240538d2e0
Chetan Rathod <chetan0101career@gmail.com>
2023-12-31 19:06:46 +0530

    deploy config

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -128,13 +128,14 @@ mimetypes.add_type("text/html", ".js", True)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
-# STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
