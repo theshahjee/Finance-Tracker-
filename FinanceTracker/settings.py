a5714329b6ba81afa4913c145e3ce43cc7cb1c62
theshahjee <abdullah5mughees@gmail.com>
2024-09-19 17:41:33 +0500

    Requirements Updated

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -144,3 +144,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AWS_ACCESS_KEY_ID = 'AKIAU6GDYMHIC2MBZYWZ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'financetrackerdjango'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
